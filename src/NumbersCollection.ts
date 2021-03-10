af99de93d29ff04641f460927c49161ba6ef3aef
lukedxvxes <lukedaviesweb+1@gmail.com>
2021-03-10 14:43:40 +0800

    first commit

---
+++ src/NumbersCollection.ts
@@ -0,0 +1,17 @@
+export class NumbersCollection {
+    constructor(public data: number[]) {}
+    //get keyword lets you access the length as a property and not call it as a method
+    get length(): number {
+        return this.data.length;
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        return this.data[leftIndex] > this.data[rightIndex];
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        const leftHand = this.data[leftIndex];
+        this.data[leftIndex] = this.data[rightIndex];
+        this.data[rightIndex] = leftHand;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {Sorter} from './Sorter';
+import {NumbersCollection} from './NumbersCollection';
+import {CharactersCollection} from './CharactersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection('luke');
+
+const sorter = new Sorter(charactersCollection);
+
+sorter.sort();
+
+console.log(charactersCollection.data);
